fbf00d3203c3fb0ec395cade81190515842aee1f
Victor San Kho Lin <victorlin21@gmail.com>
2018-02-13 20:07:03 +1100

    Making locations.file exist check more robust.

--- gen_loc.py
+++ gen_loc.py
@@ -1,9 +1,16 @@
 import os
+import os.path
+
 basePath = "./repos"
 basePath2 = "/repos"
+locations_file = "./conf.d/locations.file"
+
+if not os.path.exists(locations_file):
+    fh = open(locations_file, "w")
+    fh.close()
 
 try:
-    listingFile = open("./conf.d/locations.file", "r")
+    listingFile = open(locations_file, "r")
     listingContents = listingFile.read()
 except IOError:
     listingContents = ""
@@ -13,16 +20,12 @@ allList = os.listdir(basePath)
 for f in allList:
 	if os.path.isdir(basePath+"/" + f) and f[0]!='.':
 		if f not in listingContents:
-			fh = open("./conf.d/locations.file", "a")
+			fh = open(locations_file, "a")
 			fh.write("\nlocation " + "/"+ f +" {\n")
 			fh.write("\talias " + basePath2 +"/"+ f +"/_site;\n")
 			fh.write("\tauth_basic \"Restricted Content\";\n")
  			fh.write("\tauth_basic_user_file " + basePath2 + "/" + f + "/.htpasswd;\n")
 			fh.write("}\n")
 			fh.close()
-
-if len(allList) == 0:
-    fh = open("./conf.d/locations.file", "w")
-    fh.close()
 
 print "Done"
