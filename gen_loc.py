37f37ccdac7a1948ff7cdc11b3369fe496959b58
Victor San Kho Lin <victorlin21@gmail.com>
2018-02-14 00:39:32 +1100

    Added fail-safe to run the jekyll build command.

--- gen_loc.py
+++ gen_loc.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import shlex
+from subprocess import Popen, PIPE
 
 basePath = "./repos"
 basePath2 = "/repos"
@@ -18,14 +20,21 @@ except IOError:
 
 allList = os.listdir(basePath)
 for f in allList:
-	if os.path.isdir(basePath+"/" + f) and f[0]!='.':
-		if f not in listingContents:
-			fh = open(locations_file, "a")
-			fh.write("\nlocation " + "/"+ f +" {\n")
-			fh.write("\talias " + basePath2 +"/"+ f +"/_site;\n")
-			fh.write("\tauth_basic \"Restricted Content\";\n")
- 			fh.write("\tauth_basic_user_file " + basePath2 + "/" + f + "/.htpasswd;\n")
-			fh.write("}\n")
-			fh.close()
+    site_path = basePath + "/" + f
+    if os.path.isdir(site_path) and f[0]!='.':
+
+        command = "jekyll build -s " + site_path + " -d " + site_path + "/_site" + " -b /" + f
+        print command
+        process = Popen(shlex.split(command), stdout=PIPE, stderr=PIPE)
+        out, err = process.communicate()
+
+        if f not in listingContents:
+            fh = open(locations_file, "a")
+            fh.write("\nlocation " + "/"+ f +" {\n")
+            fh.write("\talias " + basePath2 +"/"+ f +"/_site;\n")
+            fh.write("\tauth_basic \"Restricted Content\";\n")
+            fh.write("\tauth_basic_user_file " + basePath2 + "/" + f + "/.htpasswd;\n")
+            fh.write("}\n")
+            fh.close()
 
 print "Done"
