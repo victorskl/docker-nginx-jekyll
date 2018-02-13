c4796f2339eab6ceb5a88b4bfc2dafe1d8b8b71d
Victor San Kho Lin <victorlin21@gmail.com>
2018-02-13 17:46:47 +1100

    Exclude start with .directory. Able to create empty locations.file

--- gen_loc.py
+++ gen_loc.py
@@ -11,7 +11,7 @@ except IOError:
 
 allList = os.listdir(basePath)
 for f in allList:
-	if os.path.isdir(basePath+"/" + f):
+	if os.path.isdir(basePath+"/" + f) and f[0]!='.':
 		if f not in listingContents:
 			fh = open("./conf.d/locations.file", "a")
 			fh.write("\nlocation " + "/"+ f +" {\n")
@@ -20,3 +20,9 @@ for f in allList:
  			fh.write("\tauth_basic_user_file " + basePath2 + "/" + f + "/.htpasswd;\n")
 			fh.write("}\n")
 			fh.close()
+
+if len(allList) > 0:
+    fh = open("./conf.d/locations.file", "a")
+    fh.close()
+
+print "Done gen_loc.py!"
